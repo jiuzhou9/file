4f43e0696a1fce1ecf25e9b7843d7cc266fba815
jiuzhou999 <835540436@qq.com>
2017-07-15 02:39:39 +0800

    Initial commit

---
+++ file-domain/src/main/java/cn/jiuzhou/file/App.java
@@ -0,0 +1,13 @@
+package cn.jiuzhou.file;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
